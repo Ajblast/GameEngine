176817818e9fa444f784c3b8a2a5eec6468251b5
Austin Kincer <austinkincer@gmail.com>
2021-01-30 01:42:47 -0500

    Implemented job system testing

--- test/src/TestFile.cpp
+++ test/src/TestFile.cpp
@@ -1,82 +1,120 @@
 #include "common.h"
 
-#include "logging/sinks/sinks.h"
+//#define NOMINMAX
+//#include <Windows.h>
+//#include <PathCch.h>
+//#include <Shlwapi.h>
 
-#define NOMINMAX
-#include <Windows.h>
-#include <PathCch.h>
-#include <Shlwapi.h>
+#include "jobs/jobs.h"
+#include "jobs/jobManager.h"
+#include "logging/sinks/sinks.h"
 
 #include <iostream>
 
-GRAVEngine::Logging::logManager manager;
+GRAVEngine::Logging::logManager logManager;
+GRAVEngine::jobs::jobManager jobManager;
+
+
+void endingJob(uintptr_t)
+{
+	GRAV_LOG_LINE_CRITICAL("This is a job to start shutting down the job manager");
+	GRAVEngine::jobs::jobManager::getInstance()->startShutdown();
+}
+void waitJob(uintptr_t)
+{
+	int counter = 10;
+	while (counter != 0)
+	{
+		GRAV_LOG_LINE_CRITICAL("Waiting...");
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		counter--;
+	}
+	GRAV_LOG_LINE_CRITICAL("After waiting");
+	
+	GRAVEngine::jobs::jobManager::getInstance()->kickJob({ endingJob, 0,  GRAVEngine::jobs::jobPriority::HIGH, nullptr });
+}
+
+
+void mainMethod()
+{
+	GRAVEngine::jobs::jobManager::getInstance()->kickJob({ waitJob, 0,  GRAVEngine::jobs::jobPriority::NORMAL, nullptr });
+
+
+	auto job = [](uintptr_t value)
+	{
+		GRAV_LOG_LINE_CRITICAL("This is a test job entry point: %d", value);
+	};
+	GRAVEngine::jobs::counter* counter = new GRAVEngine::jobs::counter();
+
+	GRAV_LOG_LINE_CRITICAL("Kick Jobs");
+	for (size_t i = 0; i < 30; i++)
+	{
+		// Kick a job
+		GRAVEngine::jobs::jobManager::getInstance()->kickJob({ job, i,  GRAVEngine::jobs::jobPriority::NORMAL, counter });
+	}
+	GRAV_LOG_LINE_CRITICAL("After Kick Job");
+
+	//GRAVEngine::jobs::jobManager::getInstance()->waitForCounter(counter, 0);
+	//GRAV_LOG_LINE_CRITICAL("After Waiting on counter");
+	delete counter;
+}
 
 int main()
 {
 #pragma region PathStuff
-	//wchar_t pathBuffer[MAX_PATH_LENGTH];
-	char pathBuffer[MAX_PATH_LENGTH];
+	////wchar_t pathBuffer[MAX_PATH_LENGTH];
+	//char pathBuffer[MAX_PATH_LENGTH];
 
-	//wchar_t* string = (wchar_t*) GetModuleFileName(nullptr, pathBuffer, sizeof(pathBuffer) / 2);
-	GetModuleFileNameA(nullptr, pathBuffer, sizeof(pathBuffer) / 2);
-	//PathCchRemoveFileSpec(string, MAX_PATH_LENGTH);
+	////wchar_t* string = (wchar_t*) GetModuleFileName(nullptr, pathBuffer, sizeof(pathBuffer) / 2);
+	//GetModuleFileNameA(nullptr, pathBuffer, sizeof(pathBuffer) / 2);
+	////PathCchRemoveFileSpec(string, MAX_PATH_LENGTH);
 
-	char dirBuffer[MAX_PATH_LENGTH];
+	//char dirBuffer[MAX_PATH_LENGTH];
 
-	_splitpath(pathBuffer, nullptr, dirBuffer, nullptr, nullptr);
+	//_splitpath(pathBuffer, nullptr, dirBuffer, nullptr, nullptr);
 
-	std::stringstream ss;
-	ss << dirBuffer;
-	ss << "testFile.txt";
+	//std::stringstream ss;
+	//ss << dirBuffer;
+	//ss << "testFile.txt";
 
-	std::cout << ss.str().c_str() << std::endl;
+	//std::cout << ss.str().c_str() << std::endl;
 
-	std::string filePath = ss.str();
+	//std::string filePath = ss.str();
 #pragma endregion
 
+#pragma region SettingUpLogging
+	// Create a default logger with a file sink and a sink to the msvc console
+	auto fileSink = std::make_shared<GRAVEngine::Logging::Sinks::fileSink>("A:\\Users\\Austin Kincer\\Source\\Repos\\Ajblast\\GameEngine\\test\\test.txt");
+	auto msvcSink = std::make_shared<GRAVEngine::Logging::Sinks::msvcSink>();
+	auto coutSink = std::make_shared<GRAVEngine::Logging::Sinks::ostreamSink>(std::cout, true);
 
-	//GRAVEngine::Logging::Sinks::fileSink fileSink(filePath.c_str());
-	
-	//const char* buffer = "This is a test buffer.";
-	//char* readBuffer = new char[100];
-	//size_t bufferSize;
-	//
-	//GRAVEngine::io::file testFile(ss.str().c_str(), GRAVEngine::io::fileMode::read, true);
-	//std::cout << "File Size: " << testFile.fileSize() << std::endl;;
-	//testFile.readAll((void*&)readBuffer, bufferSize);
-	//
-	//readBuffer[bufferSize] = 0;
-	//
-	//std::cout << "File Contents: " << readBuffer << std::endl;
-	//
-	//// Reopen the file in append mode
-	//testFile.reopen(GRAVEngine::io::fileMode::append);
-	//
-	//testFile.write((void*) buffer, strlen(buffer));
-	//testFile.flush();
+	GRAVEngine::Logging::logger logger = GRAVEngine::Logging::logger("Default", { fileSink, msvcSink, coutSink });
+	// Have to set the verbosity when providing a logger for startup
+	logger.setVerbosity(GRAVEngine::Logging::verbosity::trace);
+	logger.setFlushVerbosity(GRAVEngine::Logging::verbosity::trace);
 
-	auto testFile = GRAVEngine::io::file(ss.str().c_str(), GRAVEngine::io::fileMode::append, false);
-	std::cout << "File Path: " << testFile.filePath() << std::endl;
-	testFile.close();
+	// Start up the manager
+	logManager.startUp(std::make_shared<GRAVEngine::Logging::logger> (logger));
+	GRAV_LOG_LINE_INFO("%s: Log Manager Initialized", __FUNCTION__);
+#pragma endregion
 
-	manager.startUp({ std::make_shared<GRAVEngine::Logging::Sinks::fileSink>(ss.str().c_str()) });
-	//auto test = std::make_shared<GRAVEngine::Logging::Sinks::fileSink>(ss.str().c_str(), false);
-	//
-	//GRAVEngine::Logging::logMessage message("", GRAVEngine::Logging::verbosity::critical, "This is a test payload");
-	//
-	//std::cout << "Test File Path: " << test->filePath() << std::endl;
-	//
-	//test->log(message);
+	GRAV_LOG_LINE_INFO("%s: Begin starting up managers", __FUNCTION__);
+
+#pragma region SettingUpJobManager
+	// Use default job manager options
+	GRAVEngine::jobs::jobManagerOptions jobManagerOptions;
+	jobManagerOptions.m_ShutDownAfterMain = false;
+
+	// Start up the job manager
+	jobManager.startUp(jobManagerOptions);
+	GRAV_LOG_LINE_INFO("%s: Job Manager Initialized", __FUNCTION__);
+#pragma endregion
+
+	jobManager.runMain(mainMethod);
 
 
-
-
-	// manager.defaultLogger()->sinks().front();
-
-	manager.defaultLogger()->log(GRAVEngine::Logging::verbosity::critical, "This is a test", "");
-	manager.defaultLogger()->flush();
-
-	// Test Assertion
-	GRAV_ASSERT(1 == 2);
+	GRAV_LOG_LINE_INFO("%s: Begin shutting down managers", __FUNCTION__);
+	jobManager.shutDown();
+	logManager.shutDown();
 }
 
