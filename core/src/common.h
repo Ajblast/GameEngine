905356cef9b034fac4bfa911e8a4ee9d359a190e
Austin Kincer <austinkincer@gmail.com>
2021-02-19 05:35:57 -0500

    Added debug maco and move platform detection

--- core/src/common.h
+++ core/src/common.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "platform.h"
+
 #define MAX_EXCEPTION_STRING_LENGTH 1024
 #define MAX_LOG_BUFFER_SIZE 1024
 #define MAX_PATH_LENGTH 260
@@ -8,12 +10,16 @@
 #define ASSERTIONS_ENABLED
 #endif
 
+#ifdef _DEBUG
+#define GRAV_DEBUG
+#endif
+
 #define BIT(x) (1 << x)
 
 #define GRAV_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 // TODO: Change this new to allow tracking of memory allocations
-
+// TODO: Get rid of this function and the const char* and just use std::string. Why did I even use them in the first place??
 #define STRING_COPY(destination, source, maxSize) \
 size_t stringLength = strlen(source) + 1; \
 stringLength = std::min(stringLength, (size_t)maxSize); \
@@ -23,9 +29,7 @@ strncpy(tmp, source, stringLength); \
 tmp[stringLength - 1] = 0; \
 destination = tmp
 
-#include "platform.h"
 #include "dataPrimitives.h"
 #include "assertion.h"
 #include "errorCodes.h"
 #include "logging/logging.h"
-#include "time/time.h"
