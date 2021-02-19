86b2ca200ea1fbea243498c9143d5332d93ee15b
Austin Kincer <austinkincer@gmail.com>
2021-02-19 05:36:29 -0500

    Added references to functionality that should be accessible to applicaitons

--- core/src/GRAVEngine.h
+++ core/src/GRAVEngine.h
@@ -1,10 +1,23 @@
 #pragma once
 
+// These are items that should be accessible to applications
+
+// Common item that are used everywhere and should always be accessible
 #include "common.h"
 
 #include "application.h"
+#include "layers/layer.h"
 #include "logging/logging.h"
 #include "assertion.h"
 
-#include "keyCodes.h"
-#include "mouseCodes.h"
+// Time
+#include "time/stopwatch.h"
+#include "time/timestep.h"
+
+// Input
+#include "io/input.h"
+#include "io/keyCodes.h"
+#include "io/mouseCodes.h"
+
+// ImGui
+#include "layers/imgui/imguiLayer.h"
