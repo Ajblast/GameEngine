7cba567a6e95d03e05ec5c84de14f96ee8753ff8
Ajblast <46971018+Ajblast@users.noreply.github.com>
2021-10-17 08:29:28 -0400

    Added loss stats

--- core/src/ai/training/ITrainingAlgorithm.h
+++ core/src/ai/training/ITrainingAlgorithm.h
@@ -7,6 +7,7 @@
 #include "networkSettings.h"
 #include "hyperparameters.h"
 #include "ai/inferenceDevice.h"
+#include "ai/agentEpisodeId.h"
 #include <sstream>
 
 namespace GRAVEngine
@@ -15,6 +16,14 @@ namespace GRAVEngine
 	{
 		namespace Training
 		{
+			// Small struct to get the different loss items
+			struct updateLoss
+			{
+				float m_ActorLoss;
+				float m_CriticLoss;
+				float m_TotalLoss;
+			};
+
 			// Interface for a training algorithm
 			class GRAVAPI ITrainingAlgorithm
 			{
@@ -23,8 +32,6 @@ namespace GRAVEngine
 				virtual ~ITrainingAlgorithm() = default;
 
 			public:
-				// Get the model
-				//virtual Models::model& model() = 0;
 				// Get the algorithm type
 				virtual algorithmType getAlgorithmType() = 0;
 				// Get the network settings
@@ -35,28 +42,30 @@ namespace GRAVEngine
 				virtual bool shouldUpdate() = 0;
 				// Send the algorithm to a inference device
 				virtual void sendToDevice(inferenceDevice device) = 0;
-				//// Is the algorithm initialized
-				//virtual bool initialized() = 0;
-			
-			public:
+
+				// Get the training algorithm's model
+				virtual torch::nn::AnyModule getModel() = 0;
+				// Get the training algorithm's optimizer
+				virtual torch::optim::Optimizer& getOptimizer() = 0;
+
+				//// Get the cumulative rewards for all of the agents
+				//virtual std::unordered_map<agentEpisodeId, float> getCumulativeRewards() = 0;
+
 				// Run through the algorithm's model
 				virtual Models::ActorCritic::actorCriticOputput forward(std::vector<torch::Tensor> inputs) = 0;
+				// Add a trajectory to the algorithm
+				virtual void addTrajectory(trajectory trajectory) = 0;
 
 				// Print the model
 				virtual void print() const = 0;
 
 				// Update the model with a trajectory
-				virtual void update() = 0;
-				//// Create a model for the algorithm
-				//virtual ref<Models::model> createModel() = 0;
+				virtual updateLoss update() = 0;
+
 				// Save the model to filePath
 				virtual void saveModel(const std::string& filePath) = 0;
 				// Load a model from filePath
 				virtual void loadModel(const std::string& filePath) = 0;
-				// Add a trajectory to the algorithm
-				virtual void addTrajectory(trajectory trajectory) = 0;
-				//// Initialize the training algorithm
-				//virtual void initialize(networkSettings settings, ref<hyperparameters> parameters) = 0;
 			};
 		}
 	}
