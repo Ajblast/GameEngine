5f274e7f77a9581956ae5c926ca87b69e22f264c
Ajblast <46971018+Ajblast@users.noreply.github.com>
2021-10-17 08:29:11 -0400

    Increase batch and buffer size

--- core/src/ai/training/hyperparameters.h
+++ core/src/ai/training/hyperparameters.h
@@ -17,8 +17,8 @@ namespace GRAVEngine
 
 			public:
 				size_t m_MaxSteps = 5000000;	// Maximum number of steps
-				size_t m_BatchSize = 64;		// Size of a batch
-				size_t m_BufferSize = 640;		// Size the update buffer needs to be before training will occur
+				size_t m_BatchSize = 128;		// Size of a batch
+				size_t m_BufferSize = 4096;		// Size the update buffer needs to be before training will occur
 				size_t trajectoryHorizon = 64;	// How many observations before a trajectory is filled and should be cut off
 				float m_Gamma = .99f;			// How much future opbservations should influence compared to recent observations
 				float m_LearningRate = 3e-4f;	// Learning rate
